6bff2b3effcfc82799a757505a967d4cde07b2c8
trkviktor <iroboteditor@gmil.com>
2022-02-24 13:22:42 +0100

    easier and better calc

--- szamologep/Main.java
+++ szamologep/Main.java
@@ -7,44 +7,33 @@ public class Main {
 	static Scanner in = new Scanner(System.in);
 	
 	public static void main(String[] args) {
-		int nmbr1 = read(0,100);
-		char Op = read();
-		int nmbr2 = read(0,100);
-		if(Op == '/' && (nmbr2==0)) {
-			System.out.println("You cant divide by zero!");
-			nmbr2 = read(0,100);
+		repeatFunction();
+	}
+	
+	
+	public static void repeatFunction() {
+		boolean ok;
+		String inp;
+		do {
+			System.out.println("Desired equation: ");
+			inp = in.nextLine();
+			String[] strArray = inp.split(" ");
+			ok = true;
+		try {
+			int a = Integer.parseInt(strArray[0]);
+			char op = strArray[1].charAt(0);
+			int b = Integer.parseInt(strArray[2]);
+			System.out.printf("%d %c %d = %d",a,op,b,Calc(a,b,op));
+			
 		}
-		System.out.printf("%d %c %d = %d",nmbr1,Op,nmbr2,Calc(nmbr1,nmbr2,Op));
-	}
-	
-	public static int read(int min,int max) {
-		int number = 0;
-		do {
-			System.out.printf("\nEnter a number between: %d and %d:",min,max);
-			while(!in.hasNextInt()) {
-				System.out.printf("\nThe number must be between %d and %d:",min,max);
-				in.next();
-			}
-			number = in.nextInt();
-			in.nextLine();
-		}while(number < min || number > max);
-		return number;
-	}
-	
-	public static char read() {
-		char Operator;
-		boolean good = false;
-		String Operators = "+-*/";
-		do {
-			System.out.println("Enter a basic operator(+-*/):");
-			Operator = in.next().charAt(0);
-			in.nextLine();
-			for(int i = 0; i<Operators.toCharArray().length;i++){
-				if(Operator == Operators.charAt(i))	
-					good = true;
-			}
-		}while(!good);
-		return Operator;
+		catch (Exception e) {
+			ok = false;
+		}
+		}while(!ok);
+		
+		
+		
+		
 	}
 	
 	public static int Calc(int nmbr1,int nmbr2,char Operator) {
